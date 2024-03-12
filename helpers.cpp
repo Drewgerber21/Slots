4cc0dc32346306b10bdf2d01d681db9e1cc67af5
Drew Gerber <dggerber@csu.fullerton.edu>
2024-03-11 19:18:38 -0700

    Added randomizeVector function to use for the spin mechanic

---
+++ helpers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "helpers.h"
+
+#include <algorithm>
+#include <random>
+
+using namespace HELPERS;
+
+void HELPERS::randomizeVector(std::vector<char>& vector)
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::shuffle(vector.begin(), vector.end(), gen);
+}
+
---
+++ helpers.h
@@ -0,0 +1,12 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#include <iostream>
+#include <vector>
+
+namespace HELPERS
+{
+	void randomizeVector(std::vector<char>& vector);
+}
+
+#endif 
